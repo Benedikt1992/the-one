47d30e0761f3e62199684a659fa81d31167637ac
Benedikt Bock <mail@benedikt1992.de>
2020-01-06 16:51:59 +0100

    read complete file once

--- python-tools/osm2wkt/osm2wkt.py
+++ python-tools/osm2wkt/osm2wkt.py
@@ -3,7 +3,7 @@ from decimal import *
 from geopy import distance
 
 
-tree = ET.parse("test.osm")
+tree = ET.parse("export.osm")
 root = tree.getroot()
 
 # latitude is horizontal from -90 -- 0 -- 90
@@ -31,25 +31,25 @@ else:
                 maxlon = Decimal(item.attrib['lon'])
             if Decimal(item.attrib['lon']) < minlon:
                 minlon = Decimal(item.attrib['lon'])
-# TODO serialize file access
 # todo use multiprocessing
 
-with open('test.wkt', 'w') as output:
-    node_cache = {}
+# nodes = {id: (lat,lon,x,y)}
+nodes = {}
+for node in root.findall('node'):
+    nodes[node.get('id')] = (Decimal(node.get('lat')), Decimal(node.get('lon')), None, None)
+
+with open('export.wkt', 'w') as output:
     for way in root.findall('way'):
         output.write("LINESTRING (")
         waypoints = []
         for point in way.findall('nd'):
-            node = root.find("node[@id='{}']".format(point.get('ref')))
-            if node is None:
-                raise ArithmeticError("Node {} not found".format(point.get('ref')))
-            x, y = node_cache.get(node.get('id'), (None, None))
-            if x is None and y is None:
-                x = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
-                                      (Decimal(node.get('lat')), minlon)).meters
-                y = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
-                                      (minlat, Decimal(node.get('lon')))).meters
-                node_cache[node.get('id')] = (x, y)
+            node = nodes[point.get('ref')]
+            if node[2] is None and node[3] is None:
+                x = distance.distance((node[0], node[1]),
+                                      (node[0], minlon)).meters
+                y = distance.distance((node[0], node[1]),
+                                      (minlat, node[1])).meters
+                nodes[point.get('ref')] = (node[0], node[1], x, y)
             waypoints.append("{} {}".format(x, y))
         output.write(", ".join(waypoints))
         output.write(")\n")
