32860dcf78aa09bedc4653e27e1b7ae8cea1170c
Benedikt Bock <mail@benedikt1992.de>
2020-01-06 15:10:30 +0100

    use cache for nodes

--- python-tools/osm2wkt/osm2wkt.py
+++ python-tools/osm2wkt/osm2wkt.py
@@ -31,6 +31,8 @@ else:
                 maxlon = Decimal(item.attrib['lon'])
             if Decimal(item.attrib['lon']) < minlon:
                 minlon = Decimal(item.attrib['lon'])
+# TODO serialize file access
+# todo use multiprocessing
 
 with open('test.wkt', 'w') as output:
     node_cache = {}
@@ -38,16 +40,16 @@ with open('test.wkt', 'w') as output:
         output.write("LINESTRING (")
         waypoints = []
         for point in way.findall('nd'):
-            point.get('ref')
             node = root.find("node[@id='{}']".format(point.get('ref')))
-            # todo use node_cache
             if node is None:
                 raise ArithmeticError("Node {} not found".format(point.get('ref')))
-            x = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
-                                  (Decimal(node.get('lat')), minlon)).meters
-            y = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
-                                  (minlat, Decimal(node.get('lon')))).meters
-            node_cache[node.get('id')] = (x, y)
+            x, y = node_cache.get(node.get('id'), (None, None))
+            if x is None and y is None:
+                x = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
+                                      (Decimal(node.get('lat')), minlon)).meters
+                y = distance.distance((Decimal(node.get('lat')), Decimal(node.get('lon'))),
+                                      (minlat, Decimal(node.get('lon')))).meters
+                node_cache[node.get('id')] = (x, y)
             waypoints.append("{} {}".format(x, y))
         output.write(", ".join(waypoints))
         output.write(")\n")
