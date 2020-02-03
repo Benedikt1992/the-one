634e56cb9f1f6bad73dc1c56f5adde9269833aa9
Benedikt Bock <mail@benedikt1992.de>
2020-02-03 18:06:07 +0100

    Add ConstantDelay model

---
+++ src/movement/map/delay/ConstantDelay.java
@@ -0,0 +1,39 @@
+package movement.map.delay;
+
+import core.Settings;
+import movement.map.MapScheduledNode;
+import movement.map.MapScheduledRoute;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This DelayModel add a constant delay to each route.
+ * The amount of delay needs to be specified with Group.delay
+ */
+public class ConstantDelay extends DelayModel {
+    /** The amount of delay each train has right from the beginning. */
+    public static final String DELAY_S = "delay";
+    /** The amount of delay added to each route */
+    private double delay;
+    /**
+     * Creates a new DelayModel based on a Settings object's settings.
+     *
+     * @param settings The Settings object where the settings are read from
+     */
+    public ConstantDelay(Settings settings) {
+        super(settings);
+        delay = settings.getDouble(DELAY_S);
+    }
+
+    @Override
+    public MapScheduledRoute calculateDelay(MapScheduledRoute route) {
+        List<MapScheduledNode> nodes = route.getStops();
+        List<MapScheduledNode> new_nodes = new ArrayList<>();
+        for (MapScheduledNode node :
+                nodes) {
+            new_nodes.add(node.updateTime(node.getTime() + this.delay));
+        }
+        return new MapScheduledRoute(new_nodes);
+    }
+}
